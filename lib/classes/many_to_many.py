2337b73eb6d5c0c10477a5007f7b6120092b9c89
alchemist1111 <emoyoeugine@gmail.com>
2024-05-28 05:57:20 +0300

    updates

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,121 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            raise ValueError("Author must be an instance of Author class.")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be an instance of Magazine class.")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters.")
         self.author = author
         self.magazine = magazine
         self.title = title
+        author._articles.append(self)
+        magazine._articles.append(self)
+        Article.all.append(self)
+
+    # @property
+    # def title(self):
+    #     return self._title
+    
+    # @property
+    # def author(self):
+    #     return self._author
+    
+    # @property
+    # def magazine(self):
+    #     return self._magazine
+    
+    # @magazine.setter
+    # def magazine(self, value):
+    #     if not isinstance(value, Magazine):
+    #         raise ValueError("Magazine must be an instance of Magazine class.")
+    #     self._magazine = value
+
+    # @author.setter
+    # def author(self, value):
+    #     if not isinstance(value, Author):
+    #         raise ValueError("Author must be an instance of Author class.")
+    #     self._author = value    
         
 class Author:
-    def __init__(self, name):
+    def __init__(self, name=""):
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string.")
         self.name = name
+        self._articles = []
+  
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+         return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list(set(article.magazine.category for article in self._articles))
 
 class Magazine:
+    all_magazines = []
     def __init__(self, name, category):
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise ValueError("Name must be a string between 2 and 16 characters.")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Category must be a non-empty string.")
         self.name = name
         self.category = category
+        self._articles = []
+        self.all_magazines.append(self)
+
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) < 2 or len(value) > 16:
+            raise ValueError("Magazine name must be a string between 2 and 16 characters")
+        self._name = value
 
     def articles(self):
-        pass
+        return self._articles
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("Category must be a non-empty string.")
+        self._category = value
 
     def contributors(self):
-        pass
+         return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles] if self._articles else None
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            author = article.author
+            if author not in author_counts:
+                author_counts[author] = 0
+            author_counts[author] += 1
+            return [author for author, count in author_counts.items() if count > 2] or None
+        
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all_magazines:
+            return None
+        return max(cls.all_magazines, key=lambda mag: len(mag.articles()))    
+        
